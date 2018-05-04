7f192532bba872efa378bf53d773d047f31955f3
Andreev Sergey <Andreev@vazimut.ru>
2018-05-04 16:32:08 +0300

    Save to GitHub

--- src/ru/vazimut/orgeo/FileOrgeo.java
+++ src/ru/vazimut/orgeo/FileOrgeo.java
@@ -5,13 +5,20 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Scanner;
 
+import static java.lang.Boolean.*;
+
 class FileOrgeo {
+   protected static ArrayList<Person> allPerson = new ArrayList<Person>();
    protected static void read () {
        try {
            //Files.lines(Paths.get("C:\\Orgeo\\import.csv"), StandardCharsets.UTF_8).skip(1).forEach(System.out::println);
-           Files.lines(Paths.get("C:\\Orgeo\\import.csv"), StandardCharsets.UTF_8).skip(1).forEach(FileOrgeo::readLine);
+
+           Files.lines(Paths.get("C:\\Orgeo\\import.csv"), StandardCharsets.UTF_8).skip(1).forEach(FileOrgeo::lineToPerson);
+           System.out.println("Всего участников: " + allPerson.size());
+
        }
        catch(IOException ex){
            System.out.println(ex.getMessage());
@@ -27,5 +34,54 @@ class FileOrgeo {
        scan.close();
    }
 
+   protected static Person lineToPerson (String line) {
+
+       String group = "Group";
+       String surname = "Surname";
+       String name = "Name";
+       String team = "Team";
+       String city = "Team";
+       int sportIdent = 0;
+       String startDays = "0";
+       String coach = "Coach";
+       int idRequest = 0;
+       boolean statusPay = FALSE;
+       float summ = 0;
+
+       Scanner scan = new Scanner(line);
+       scan.useDelimiter(";");
+       group = scan.next();
+       scan.next();
+       surname = scan.next();
+       name = scan.next();
+       team = scan.next();
+       scan.next();
+       city = scan.next();
+       scan.close();
+       Person person = new Person(group,
+               surname,
+               name,
+               team,
+               city,
+               sportIdent,
+       startDays,
+       coach,
+       idRequest,
+       statusPay,
+       summ);
+       System.out.println(person.getGroup() + " " + person.getSurname() + " " + person.getName() + " " + person.getTeam());
+       //System.out.println(person.getSurname());
+       //System.out.println(person.getName());
+       //System.out.println(person.getTeam());
+       //System.out.println(person.getGroup());
+       //System.out.println(person.getGroup());
+       //System.out.println(person.getGroup());
+       //System.out.println(person.getGroup());
+       //System.out.println(person.getGroup());
+       //System.out.println(person.getGroup());
+       //System.out.println(person.getGroup());
+       allPerson.add(person);
+    return person;
+   }
 
 }
--- src/ru/vazimut/orgeo/Person.java
+++ src/ru/vazimut/orgeo/Person.java
@@ -1,5 +1,7 @@
 package ru.vazimut.orgeo;
 
+import static java.lang.Boolean.*;
+
 class Person {
   private String group;
   private String surname;
@@ -10,53 +12,117 @@ class Person {
   private String startDays;
   private String coach;
   private int idRequest;
-  private String status;
+  private boolean statusPay;
   private float summ;
 
-  protected Person (){}
+  protected Person (String group,
+          String surname,
+          String name,
+          String team,
+          String city,
+          int sportIdent,
+          String startDays,
+          String coach,
+          int idRequest,
+          boolean statusPay,
+          float summ){
+      this.group = group;
+      this.surname = surname;
+      this.name = name;
+      this.team = team;
+      this.city = city;
+      this.sportIdent = sportIdent;
+      this.startDays = startDays;
+      this.coach = coach;
+      this.idRequest = idRequest;
+      this.statusPay = statusPay;
+      this.summ = summ;
+  }
 
   protected void setGroup (String gr){
       this.group = gr;
   }
 
+  protected String getGroup (){
+        return this.group;
+   }
   protected void setSurname (String sn){
       this.surname = sn;
   }
 
-  private void  setName (String nm){
+    protected String getSurname (){
+        return this.surname;
+    }
+
+  protected void  setName (String nm){
       this.name = nm;
   }
+
+    protected String getName (){
+        return this.name;
+    }
 
   protected void setTeam (String tm){
       this.team = tm;
   }
 
+    protected String getTeam (){
+       return this.team;
+    }
+
   protected void setCity (String ct){
       this.city = ct;
   }
+
+    protected String getCity (){
+       return this.city;
+    }
 
   protected void setSportIdent (int si) {
       this.sportIdent = si;
   }
 
+    protected int getSportIdent () {
+       return this.sportIdent;
+    }
+
   protected void setStartDays (String sd){
       this.startDays = sd;
   }
+
+    protected String getStartDays (){
+        return this.startDays;
+    }
 
   protected void setCoach (String cch){
       this.coach = cch;
   }
 
+    protected String  getCoach (){
+       return this.coach;
+    }
+
   protected void setIdRequest (int idr){
       this.idRequest = idr;
   }
 
-  protected void setStatus (String st){
-      this.status = st;
+    protected int getIdRequest (){
+        return this.idRequest;
+    }
+
+  protected void setStatusPay (boolean st){
+      this.statusPay = st;
   }
+
+    protected boolean getStatusPay (){
+       return this.statusPay;
+    }
 
   protected void setSumm (float smm){
       this.summ = smm;
   }
 
+    protected float getSumm (){
+       return this.summ;
+    }
 }
